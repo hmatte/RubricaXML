9094517075c0320e36ab8fe8590d74ed323d539b
Matteo <45902818+hmatte@users.noreply.github.com>
2019-11-03 21:20:45 +0100

    Uploaded project
    GUI basements works. Delete and print file works.

---
+++ src/rubricaxml/RubricaXMLmain.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubricaxml;
+
+/**
+ *
+ * @author matte
+ */
+public class RubricaXMLmain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nome = "marco";
+        String cognome = "lupo";
+        String num_tel = "3422233456";
+        
+    }
+    
+}
